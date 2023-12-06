30664c6e3303f6d3dc68449d4be3f579709860c2
KiritoVV <113103472+KiritoVV@users.noreply.github.com>
2023-12-06 12:44:02 +0000

    add ing some custom post process data and a scripttable render pass

---
+++ Assets/Scripts/CustomPostProcessRendererFeature.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class CustomPostProcessRendererFeature : ScriptableRendererFeature
+{
+    // Start is called before the first frame update
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        
+    }
+
+    public override void 
+    {
+
+    }
+}
