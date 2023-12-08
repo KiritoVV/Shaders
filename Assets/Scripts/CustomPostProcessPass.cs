592e13795f3fcdbd72f3f06f03e528b930f674c2
KiritoVV <113103472+KiritoVV@users.noreply.github.com>
2023-12-08 15:46:10 +0000

    Continued working on the custompostprocesspass

--- Assets/Scripts/CustomPostProcessPass.cs
+++ Assets/Scripts/CustomPostProcessPass.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
@@ -10,14 +11,62 @@ public class CustomPostProcessPass : ScriptableRenderPass
     private Material m_bloomMaterial;
     private Material m_compositeMaterial;
 
+    const int k_MaxPyramidSize = 16;
+    private int[] _BloomMipUp;
+    private int[] _BloomMipDown;
+    private RTHandle[] m_BloomMipUp;
+    private RTHandle[] m_BloomMipDown;
+    private GraphicsFormat hdrFormat;
+
     public CustomPostProcessPass(Material bloomMaterial, Material compositeMaterial)
     {
         m_bloomMaterial = bloomMaterial;
         m_compositeMaterial = compositeMaterial;
+
+        renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
+
+        _BloomMipDown = new int[k_MaxPyramidSize];
+        _BloomMipDown = new int[k_MaxPyramidSize];
+        m_BloomMipUp = new RTHandle[k_MaxPyramidSize];
+        m_BloomMipDown = new RTHandle[k_MaxPyramidSize];
+
+        for (int i = 0; i < k_MaxPyramidSize; i++)
+        {
+            _BloomMipUp[i] = Shader.PropertyToID("_BLoomMipUp" + i);
+            _BloomMipDown[i] = Shader.PropertyToID("_BloomMipDown" + i);
+
+            m_BloomMipUp[i] = RTHandle.Alloc(_BloomMipUp[i], name: "BloomMipUp" + i);
+            m_BloomMipDown[i] = RTHandle.Alloc(m_BloomMipDown[i], name: "_BloomMipDown" + i);
+
+        }
+
+        const FormatUsage usage = FormatUsage.Linear | FormatUsage.Render;
+        if (SystemInfo.IsFormatSupported(GraphicsFormat.B10G11R11_UFloatPack32, usage))
+        {
+            hdrFormat = GraphicsFormat.B10G11R11_UFloatPack32;
+        }
+        else
+        {
+            hdrFormat = QualitySettings.activeColorSpace == ColorSpace.Linear
+                ? GraphicsFormat.R8G8B8A8_SRGB
+                : GraphicsFormat.R8G8B8A8_UNorm;
+
+        }
     }
 
     public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
     {
        
+    }
+
+    public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
+    {
+        m_Descriptor = renderingData.cameraData.cameraTargetDescriptor;
+    }
+
+    public void SetTarget(RTHandle cameraColorTargetHandle, RTHandle cameraDepthTargetHandle)
+    {
+        m_CameraColorTarget = cameraColorTargetHandle;
+        m_CameraDepthTarget = cameraDepthTargetHandle;
     }
 }
